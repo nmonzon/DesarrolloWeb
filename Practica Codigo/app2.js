e2f933d7582f5d98630c5dd9292874c22e24c678
NicoMonzon <nicolasmonzon@appstractweb.com>
2019-02-14 11:24:17 -0300

    Completado Seccion 4 clase 17

---
+++ Practica Codigo/app2.js
@@ -0,0 +1,27 @@
+//CLASE 17 - funciones y expresiones de funciones
+                                      //funciones
+function saludar(){
+  console.log("Hi")
+}
+
+saludar();
+//las funciones son objetos, un tipo especial. Por lo cual asi como se pasan
+//variables por funciones como parametros, tambien se pueden pasar funciones:
+
+function logSaludo(fn){
+  fn();
+}
+
+logSaludo(saludar); //paso una funcion como parametro y con fn(); la ejecuto.
+                                      //funcions expressions
+var miSaludo = function(){
+  console.log("Hola Nico");
+}
+miSaludo(); //aca estoy invocando mi funcion expresion
+logSaludo(miSaludo)
+
+
+//otro ejemplo de function expression
+logSaludo(function(){
+  console.log("Hola Lucas");
+})
